61085a29c8c86358c4ffc02f74b07c71086fc0e1
jeffrey <jeffrey2545@gmail.com>
2023-07-15 11:29:17 +0000

    Modified root URL

--- service/routes.py
+++ service/routes.py
@@ -18,10 +18,12 @@ from . import app
 @app.route("/")
 def index():
     """ Root URL response """
-    return (
-        "Reminder: return some useful information in json format about the service here",
-        status.HTTP_200_OK,
-    )
+    info = {
+        "name": "Customer Service",
+        "version": "1.0.0",
+        "List resource URL": url_for("list_customers", _external=True)
+    }
+    return jsonify(info), status.HTTP_200_OK
 
 ######################################################################
 #  R E S T   A P I   E N D P O I N T S
