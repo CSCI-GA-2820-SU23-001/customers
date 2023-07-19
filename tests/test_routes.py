c569018e9119072b11ca89f162a9ebee9b98aec8
nikhilyadavnyu <nby2003@nyu.edu>
2023-07-19 16:41:17 +0530

    Lint

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -207,10 +207,12 @@ class TestYourResourceServer(TestCase):
         customer.name = 'Valentina'
         response = self.client.patch(f'{BASE_URL}/{customer.id}', json=customer.serialize())
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-        
+
     def test_health(self):
         """It should get the health endpoint"""
         resp = self.client.get("/health")
         self.assertEqual(resp.status_code, 200)
         data = resp.get_json()
         self.assertEqual(data["status"], "OK")
+
+        
