150b8a094440b6eb7a8cffcdbe4c50f89f931276
iamAlicialin <3262312650@qq.com>
2023-08-17 01:27:58 +0000

    error fixing

--- service/__init__.py
+++ service/__init__.py
@@ -43,11 +43,11 @@ app.logger.info(70 * "*")
 app.logger.info("  S E R V I C E   R U N N I N G  ".center(70, "*"))
 app.logger.info(70 * "*")
 
-try:
-    models.init_db(app)  # make our SQLAlchemy tables
-except Exception as error:  # pylint: disable=broad-except
-    app.logger.critical("%s: Cannot continue", error)
-    # gunicorn requires exit code 4 to stop spawning workers when they die
-    sys.exit(4)
+# try:
+models.init_db(app)  # make our SQLAlchemy tables
+# except Exception as error:  # pylint: disable=broad-except
+#     app.logger.critical("%s: Cannot continue", error)
+#     # gunicorn requires exit code 4 to stop spawning workers when they die
+#     sys.exit(4)
 
 app.logger.info("Service initialized!")
